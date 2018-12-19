ac66a1cb7322b08623df2d25234eb90e24aa7176
Felix Breidenstein <felix@codemonauts.com>
2018-12-19 17:55:05 +0100

    Use gunicorn instead of flask-dev server. Fixes #14

--- entry.sh
+++ entry.sh
@@ -1,4 +1,7 @@
-#! /bin/sh
+export FLASK_APP="run.py"
 
-pipenv run python create_db.py
-pipenv run python run.py
+# Apply latest database migrations
+pipenv run flask db upgrade
+
+# Start Gunicorn
+pipenv run gunicorn -w 2 -b 127.0.0.1:5000 run:app
